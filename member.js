643237c96b05f79fe9439240306f1e3d695d3f89
yannp-atos <71124149+yannp-atos@users.noreply.github.com>
2023-11-29 21:08:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    var memberBtn2 = document.getElementById("memberBtn2");
+    var skillsBtn2 = document.getElementById("skillsBtn2");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#fff";
+    memberBtn.style.color = "#000";
+    skillsBtn.style.backgroundColor = "#000";
+    skillsBtn.style.color = "#fff";
+    memberBtn2.style.backgroundColor = "#fff";
+    memberBtn2.style.color = "#000";
+    skillsBtn2.style.backgroundColor = "#000";
+    skillsBtn2.style.color = "#fff";
+}
